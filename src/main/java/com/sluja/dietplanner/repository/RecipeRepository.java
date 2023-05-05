472f66f4b24f93fe3b85f25987ccc9a1ed0bda11
slujakrzysztof <krzysiek.sluja@vp.pl>
2023-05-05 23:48:33 +0200

    Add database configuration and repositories

---
+++ src/main/java/com/sluja/dietplanner/repository/RecipeRepository.java
@@ -0,0 +1,9 @@
+package com.sluja.dietplanner.repository;
+
+import com.sluja.dietplanner.entity.Recipe;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface RecipeRepository extends JpaRepository<Recipe, Integer> {
+}
