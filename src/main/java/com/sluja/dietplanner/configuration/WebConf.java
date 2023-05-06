d72bce00dac2c0569d642ee4190631973ac67d69
slujakrzysztof <krzysiek.sluja@vp.pl>
2023-05-06 11:41:00 +0200

    Fix project configuration

---
+++ src/main/java/com/sluja/dietplanner/configuration/WebConf.java
@@ -0,0 +1,8 @@
+package com.sluja.dietplanner.configuration;
+
+import org.springframework.boot.autoconfigure.web.servlet.WebMvcAutoConfiguration;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class WebConf extends WebMvcAutoConfiguration {
+}
--- src/main/java/com/sluja/dietplanner/controller/MainController.java
+++ src/main/java/com/sluja/dietplanner/controller/MainController.java
@@ -1,16 +1,19 @@
 package com.sluja.dietplanner.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
 
-@RestController
+@Controller
 @RequestMapping("/api/v1")
 public class MainController {
 
     @GetMapping("/main")
-    public String mainView(){
+    public String header(){
 
-        return "Works";
+        return "header";
     }
 }
