40e8e5482a658c1487ea307fc3fe6d73162b0f9a
slujakrzysztof <krzysiek.sluja@vp.pl>
2023-05-08 23:01:16 +0200

    Add language availability

--- src/main/java/com/sluja/dietplanner/configuration/WebConf.java
+++ src/main/java/com/sluja/dietplanner/configuration/WebConf.java
@@ -1,8 +1,17 @@
 package com.sluja.dietplanner.configuration;
 
 import org.springframework.boot.autoconfigure.web.servlet.WebMvcAutoConfiguration;
+import org.springframework.context.MessageSource;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.ReloadableResourceBundleMessageSource;
+import org.springframework.web.servlet.LocaleResolver;
 import org.springframework.web.servlet.config.annotation.*;
+import org.springframework.web.servlet.i18n.CookieLocaleResolver;
+import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
+import org.springframework.web.servlet.i18n.SessionLocaleResolver;
+
+import java.util.Locale;
 
 @Configuration
 public class WebConf implements WebMvcConfigurer {
@@ -10,6 +19,38 @@ public class WebConf implements WebMvcConfigurer {
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         registry.addResourceHandler("/**").addResourceLocations("classpath:/static/");
+    }
+
+    @Bean(name = "localeResolver")
+    public LocaleResolver getLocaleResolver()  {
+        SessionLocaleResolver resolver = new SessionLocaleResolver();
+        resolver.setDefaultLocale(new Locale.Builder().setLanguage("pl").setScript("Latn").setRegion("PL").build());
+        return resolver;
+    }
+
+    @Bean
+    public LocaleChangeInterceptor localeChangeInterceptor() {
+
+        LocaleChangeInterceptor lci = new LocaleChangeInterceptor();
+        lci.setParamName("lang");
+
+        return lci;
+    }
+
+    @Bean(name = "messageSource")
+    public MessageSource getMessageResource()  {
+        ReloadableResourceBundleMessageSource messageResource= new ReloadableResourceBundleMessageSource();
+
+        // Read i18n/messages_xxx.properties file.
+        // For example: i18n/messages_en.properties
+        messageResource.setBasename("classpath:lang/bundle");
+        messageResource.setDefaultEncoding("UTF-8");
+        return messageResource;
+    }
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+
+        registry.addInterceptor(localeChangeInterceptor()).addPathPatterns("/*");
     }
 
 }
