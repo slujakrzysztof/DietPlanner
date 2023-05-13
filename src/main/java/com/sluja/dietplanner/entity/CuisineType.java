02e283cdc3c9f359ede4638f49a8947444e78a4a
slujakrzysztof <krzysiek.sluja@vp.pl>
2023-05-13 23:55:46 +0200

    Add getters

--- src/main/java/com/sluja/dietplanner/entity/CuisineType.java
+++ src/main/java/com/sluja/dietplanner/entity/CuisineType.java
@@ -4,9 +4,11 @@ import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import lombok.Getter;
 
 @Entity
 @Table(name = "cuisineType")
+@Getter
 public class CuisineType {
 
     @Id
--- src/main/java/com/sluja/dietplanner/repository/FavouriteRecipeRepository.java
+++ src/main/java/com/sluja/dietplanner/repository/FavouriteRecipeRepository.java
@@ -1,9 +1,9 @@
 package com.sluja.dietplanner.repository;
 
-import com.sluja.dietplanner.entity.FavouriteRecipe;
+import com.sluja.dietplanner.entity.Recipe;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface FavouriteRecipeRepository extends JpaRepository<FavouriteRecipe, Integer> {
+public interface FavouriteRecipeRepository extends JpaRepository<Recipe, Integer> {
 }
